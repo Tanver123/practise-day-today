2883d0264a4094b116c2070c9de7aa197d521ffb
Tanver123 <alealennwalkerr@gmaii.com>
2021-10-05 09:32:57 +0600

    nothing

--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -2,11 +2,12 @@ import React from 'react';
 import "./Card.css"
 
 const Card = (props) => {
-    const { strMealThumb, strMeal } = props.meal;
+    const { strMealThumb, strMeal, strInstructions } = props.meal;
     return (
         <div className="card-container-fix">
             <h2>Name: {strMeal}</h2>
             <img src={strMealThumb} alt="" />
+            <p>{strInstructions.slice(0, 100)}</p>
 
         </div>
     );
